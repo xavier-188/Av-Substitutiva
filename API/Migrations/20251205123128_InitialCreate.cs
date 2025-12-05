b4dcc8a7c6ea923713062fd072a6d6d521bb984f
ENZO XAVIER BISCAIA <enzo.biscaia@cs.up.edu.br>
2025-12-05 10:18:07 -0300

    API Pronta

---
+++ API/Migrations/20251205123128_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Imcs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Altura = table.Column<double>(type: "REAL", nullable: false),
+                    Peso = table.Column<double>(type: "REAL", nullable: false),
+                    valorImc = table.Column<double>(type: "REAL", nullable: false),
+                    Classificacao = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imcs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Imcs");
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/api/imc/cadastrar", ([FromServices] AppDbContext ctx, [FromBody] Imc imc) =>
+{
+
+    imc.valorImc = imc.Peso / (imc.Altura * imc.Altura);
+
+    if (imc.valorImc < 18.5)
+    {
+        imc.Classificacao = "Magreza";
+    }
+    else if (imc.valorImc < 24.9)
+    {
+        imc.Classificacao = "Normal";
+    }
+    else if (imc.valorImc < 29.9)
+    {
+        imc.Classificacao = "Sobrepeso";
+    }
+    else if (imc.valorImc < 34.9)
+    {
+        imc.Classificacao = "Obesidade 1";
+    }
+    else if (imc.valorImc < 39.9)
+    {
+        imc.Classificacao = "Obesidade 2";
+    }
+    else
+    {
+        imc.Classificacao = "Obesidade 3(Grave)";
+    }
+
+    ctx.Imcs.Add(imc);
+    ctx.SaveChanges();
+    return Results.Created("", imc);
+
+});
+
+app.MapGet("/api/imc/listar", ([FromServices] AppDbContext ctx) =>
+{
+    if (ctx.Imcs.Any())
+    {
+        return Results.Ok(ctx.Imcs.ToList());
+    }
+    return Results.BadRequest("Lista vazia!");
+
+});
+
+app.MapGet("/api/imc/listar/{classificacao}", ([FromServices] AppDbContext ctx, [FromRoute] string classificacao) =>
+{
+
+    return Results.Ok(ctx.Imcs.Where(x => x.Classificacao == classificacao));
+
+});
+
+app.MapPut("/api/imc/alterar/{id}", ([FromServices] AppDbContext ctx, [FromRoute] string id, [FromBody] Imc imcAlterado) =>
+{
+    Imc imcBuscado = ctx.Imcs.Find(id);
+
+    if (imcBuscado == null)
+    {
+        return Results.NotFound("Registro não encontrado");
+    }
+
+    imcBuscado.Nome = imcAlterado.Nome;
+    imcBuscado.Altura = imcAlterado.Altura;
+    imcBuscado.Peso = imcAlterado.Peso;
+
+    imcBuscado.valorImc = imcBuscado.Peso / (imcBuscado.Altura * imcBuscado.Altura);
+
+
+    ctx.Imcs.Update(imcBuscado);
+    ctx.SaveChanges();
+    return Results.Ok("IMC Recalculado!");
+});
 
 app.Run();
